49d43f9d387506120f8a64c6bd4c22210ccf61cf
ItisCaleb <happycaleb1212@gmail.com>
2023-02-22 22:15:17 +0800

    Implement q_shuffle using Fisher-Yates shuffle

--- qtest.c
+++ qtest.c
@@ -43,6 +43,7 @@ extern int show_entropy;
  * solution code
  */
 #include "queue.h"
+#include "squeue.h"
 
 #include "console.h"
 #include "report.h"
@@ -848,6 +849,28 @@ static bool do_merge(int argc, char *argv[])
     return ok && !error_check();
 }
 
+static bool do_shuffle(int argc, char *argv[])
+{
+    if (argc != 1) {
+        report(1, "%s takes no arguments", argv[0]);
+        return false;
+    }
+
+    if (!current || !current->q)
+        report(3, "Warning: Try to access null queue");
+    error_check();
+
+    set_noallocate_mode(true);
+    if (current && exception_setup(true))
+        q_shuffle(current->q);
+    exception_cancel();
+
+    set_noallocate_mode(false);
+
+    q_show(3);
+    return !error_check();
+}
+
 
 
 static bool is_circular()
@@ -1023,6 +1046,7 @@ static void console_init()
                 "");
     ADD_COMMAND(reverseK, "Reverse the nodes of the queue 'K' at a time",
                 "[K]");
+    ADD_COMMAND(shuffle, "Ramdom shuffle the queue", "");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
